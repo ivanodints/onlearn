c6c617b6c4201c1e637519bcbcaa4af6fcfbebf5
Alexandr <trigun1000@mail.ru>
2021-10-23 01:11:44 +0300

    добавлен новый запрос в репозиторий дисциплин

--- onlearn-database/src/main/java/ru/portal/onlearn/repo/DisciplineRepository.java
+++ onlearn-database/src/main/java/ru/portal/onlearn/repo/DisciplineRepository.java
@@ -12,8 +12,13 @@ public interface DisciplineRepository extends JpaRepository<Discipline, Long> {
     @Query("select d from Discipline d where (d.title like :title or :title is null)")
     Discipline findByTitle (@Param("title") String title);
 
-    @Query(value = "SELECT * FROM Discipline ds INNER JOIN fac_disc ON ds.id = fac_disc.disc_id " +
-            "INNER JOIN Faculty f ON fac_disc.fac_id = f.id WHERE f.id = ?1", nativeQuery = true)
-    List<Discipline> findDisciplineByFacultyID (Long id);
+//  Запрос напрямую в SQL nativeQuery
+//    @Query(value = "SELECT * FROM Discipline ds INNER JOIN fac_disc ON ds.id = fac_disc.disc_id " +
+//            "INNER JOIN Faculty f ON fac_disc.fac_id = f.id WHERE f.id = ?1", nativeQuery = true)
+//    List<Discipline> findDisciplineByFacultyID (Long id);
+
+// Запрос QHL
+    @Query("select ds from Discipline ds inner join ds.faculty f where (f.id=:faculty_id or :faculty_id is null)")
+    List<Discipline> findDisciplineByFacultyID(@Param("faculty_id") Long id);
 
 }

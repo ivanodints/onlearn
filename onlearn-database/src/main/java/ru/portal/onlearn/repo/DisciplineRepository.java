a45b8bf8575d1c97dde3d92410d7b1a0eda107fb
Ivan <ivan.odintsov.1989@gmail.com>
2021-10-23 01:14:04 +0300

    Запрос к БД переделан в HQL

--- onlearn-database/src/main/java/ru/portal/onlearn/repo/DisciplineRepository.java
+++ onlearn-database/src/main/java/ru/portal/onlearn/repo/DisciplineRepository.java
@@ -10,10 +10,16 @@ import java.util.List;
 public interface DisciplineRepository extends JpaRepository<Discipline, Long> {
 
     @Query("select d from Discipline d where (d.title like :title or :title is null)")
-    Discipline findByTitle (@Param("title") String title);
+    Discipline findByTitle(@Param("title") String title);
 
-    @Query(value = "SELECT * FROM Discipline ds INNER JOIN fac_disc ON ds.id = fac_disc.disc_id " +
-            "INNER JOIN Faculty f ON fac_disc.fac_id = f.id WHERE f.id = ?1", nativeQuery = true)
-    List<Discipline> findDisciplineByFacultyID (Long id);
+// Запрос через native SQL
+
+//    @Query(value = "SELECT * FROM Discipline ds INNER JOIN fac_disc ON ds.id = fac_disc.disc_id " +
+//            "INNER JOIN Faculty f ON fac_disc.fac_id = f.id WHERE f.id = ?1", nativeQuery = true)
+//    List<Discipline> findDisciplineByFacultyID (Long id);
+
+// Запрос через HQL
+    @Query("select ds from Discipline ds inner join ds.faculty f where (f.id=:faculty_id or :faculty_id is null)")
+    List<Discipline> findDisciplineByFacultyID(@Param("faculty_id") Long id);
 
 }
